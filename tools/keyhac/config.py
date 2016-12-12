c0ccd43be17dff296a883eb4b148bc2db53b6027
Yasuhiro SHIMIZU <yasuhiro.phil81@gmail.com>
2016-12-12 02:46:49 +0000

    Add keymap for Excel.

--- tools/keyhac/config.py
+++ tools/keyhac/config.py
@@ -297,6 +297,13 @@ def configure(keymap):
         keymap_acrord[ "C-S" ] = "C-F"                  # Search
         keymap_acrord[ "C-Space" ] = "244"              # ZenkakuHankaku
 
+    if 1:
+        keymap_excel = keymap.defineWindowKeymap( exe_name="EXCEL.EXE" )
+        keymap_excel[ "C-A" ] = "Home"                  # Move to beginning of line
+        keymap_excel[ "C-H" ] = "Back"                  # Backspace
+        keymap_excel[ "C-K" ] = "S-End","C-X"           # Removing following text
+        keymap_excel[ "C-Space" ] = "244"               # ZenkakuHankaku
+
     # Customizing clipboard history list
     if 0:
         # Enable clipboard monitoring hook (Default:Enabled)
